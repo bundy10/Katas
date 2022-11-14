996f3f7312c0f0f6b2f4562bcaf91e4e15c977ad
bundy huang <bundy.huang@myob.com>
2022-11-15 05:54:58 +1100

    really wish i could of written testsuyash

--- MontyHallv2/MontyHallv2/Program.cs
+++ MontyHallv2/MontyHallv2/Program.cs
@@ -8,10 +8,6 @@ namespace MontyHallv2
 {
     internal static class Program
     {
-        private static List<Door> _doors;
-        private static IPlayer _player;
-        private const int StartIndex = 1;
-        private static readonly Random Random = new Random();
         public static void Main(string[] args)
         {
             var game = new Simulator();
