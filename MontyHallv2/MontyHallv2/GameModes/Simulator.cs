4a46e94ff68a8ae3633a07e103ca9e6f06b9de0a
bundy huang <bundy.huang@myob.com>
2022-11-15 05:13:58 +1100

    host injecting prize to door before player selection

--- MontyHallv2/MontyHallv2/GameModes/Simulator.cs
+++ MontyHallv2/MontyHallv2/GameModes/Simulator.cs
@@ -1,4 +1,5 @@
 using MontyHallV2.DoorCreation;
+using MontyHallv2.HostOperations;
 using MontyHallV2.Interfaces;
 
 namespace MontyHallV2.GameModes;
@@ -9,8 +10,8 @@ public class Simulator
     private IPlayer _player;
     private const int StartIndex = 1;
     private int _playerChoice;
-    private readonly Random _random = new Random();
-    
+    private Host _theHost = new Host();
+
 
     public Simulator(IPlayer player)
     {
@@ -18,10 +19,11 @@ public class Simulator
         _doors = Enumerable.Range(StartIndex, Door.GetCount())
             .Select(_ => new Door())
             .ToList();
-        
+
+        _doors[_theHost.InjectPrizeToDoor(_doors)].InjectCarToDoor(); // inject the prize to a door
         _player = player;
         GetPlayerDoorChoice();
-        _doors[_playerChoice].PlayerPickedDoor();
+        _doors[_playerChoice].PlayerPickedDoor(); // player picking door 
         
     }
 
--- MontyHallv2/MontyHallv2/HostOperations/Host.cs
+++ MontyHallv2/MontyHallv2/HostOperations/Host.cs
@@ -1,6 +1,15 @@
+using MontyHallV2.DoorCreation;
+
 namespace MontyHallv2.HostOperations;
 
 public class Host
 {
+    private readonly Random _random = new Random();
+    private int _prizeInjector;
     
+    public int InjectPrizeToDoor(List<Door> doors)
+    {
+        _prizeInjector = _random.Next(doors.Count);
+        return _prizeInjector;
+    }
 }
