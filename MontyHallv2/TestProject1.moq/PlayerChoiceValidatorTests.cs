35743e690faee445c5c188afc43b3e8a859dd6d1
bundy huang <bundy.huang@myob.com>
2022-12-14 17:18:03 +1100

    validation tests

---
+++ MontyHallv2/TestProject1.moq/PlayerChoiceValidatorTests.cs
@@ -0,0 +1,41 @@
+using MontyHallv2.DoorCreation;
+using MontyHallv2.GameModes;
+using MontyHallv2.Validation;
+
+namespace TestProject1.moq;
+
+public class PlayerChoiceValidatorTests
+{
+    private readonly List<Door> _doors;
+    private readonly ConsoleGame _consoleGame = new();
+
+    [Fact]
+    public void GivenPLayerChooseDoorIsCalled_WhenUserDoorChoiceIsInvalid_ThenValidatorWillReturnFalse()
+    {
+        //Arrange
+        var stringReader = new StringReader("4");
+        Console.SetIn(stringReader);
+        
+        //Act
+        var userDoorSelection = _consoleGame.GetDoorSelectionFromUser();
+        var isUserDoorSelectionValid = PlayerChoiceValidator.ValidateUserDoorSelection(userDoorSelection);
+        
+        //Assert
+        Assert.False(isUserDoorSelectionValid);
+    }
+
+    [Fact]
+    public void GivenPLayerSwitchOrStayDoorIsCalled_WhenUserInputIsInvalid_ThenValidatorWillReturnFalse()
+    {
+        //Arrange
+        var stringReader = new StringReader("c");
+        Console.SetIn(stringReader);
+        
+        //Act
+        var userSwitchOrStayChoice = _consoleGame.GetSwitchOrStayChoiceFromUser();
+        var isUserSwitchOrStayChoiceIsValid = PlayerChoiceValidator.ValidateUserSwitchOrStayChoice(userSwitchOrStayChoice);
+        
+        //Assert
+        Assert.False(isUserSwitchOrStayChoiceIsValid);
+    }
+}
