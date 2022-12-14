56af55604c63050fd25275b0cba323f1321a7508
bundy huang <bundy.huang@myob.com>
2022-12-14 14:13:43 +1100

    Refactored consolegametests to test at a lower level

--- MontyHallv2/TestProject1.moq/ConsoleGameTests.cs
+++ MontyHallv2/TestProject1.moq/ConsoleGameTests.cs
@@ -1,7 +1,7 @@
-using MontyHallV2.DoorCreation;
-using MontyHallV2.GameModes;
+using MontyHallv2.DoorCreation;
+using MontyHallv2.GameModes;
 using MontyHallv2.GameShowStaff;
-using MontyHallV2.Interfaces;
+using MontyHallv2.Interfaces;
 using MontyHallv2.Random;
 using Moq;
 
@@ -9,72 +9,120 @@ namespace TestProject1.moq;
 
 public class ConsoleGameTests
 {
-    private List<Door> _doors;
+    private readonly List<Door> _doors;
     private readonly ConsoleGame _consoleGame = new();
-    private const int CarDoorIndex = 0;
-    private const string InputFirstDoor = "1";
-    private const int SecondDoorIndex = 1;
-    private const int ThirdDoorIndex = 2;
+    private const int CarDoor = 0;
 
     public ConsoleGameTests()
     {
         //Assert
-        _doors = new GameMaster(new RandomNum()).GetDoorsIncludingCarDoors();
+        _doors = Enumerable.Range(1, 3)
+            .Select(_ => new Door())
+            .ToList();
+        
+        _doors[0].InjectCarToDoor();
     }
 
     [Fact]
     public void GivenPlayerChooseDoorIsCalled_WhenUserIsPromptedToOpenADoor_ThenADoorWillBePicked()
     {
         //Arrange
-        var stringReader = new StringReader(InputFirstDoor);
+        var stringReader = new StringReader("1");
         Console.SetIn(stringReader);
-        const int expectedOneDoorToBePicked = 1;
+        const int expectedExactlyOneDoorToBePicked = 1;
         
         //Act
         _consoleGame.PlayerChooseDoor(_doors);
         var numOfDoorsPicked = _doors.FindAll(door => door.HasPlayerPicked()).Count;
         
         //Assert
-        Assert.Equal(expectedOneDoorToBePicked, numOfDoorsPicked);
+        Assert.Equal(expectedExactlyOneDoorToBePicked, numOfDoorsPicked);
     }
     
     [Fact]
-    public void GivenPlayerChooseDoorIsCalled_WhenDoorsAreAvailable_ThenUserIsPromptedToOpenADoor()
+    public void GivenPlayerSwitchOrStayDoorIsCalled_WhenADoorIsAlreadyPicked_ThenThatDoorWillBeUnpicked()
     {
         //Arrange
-        const string inputDoorToPickAndYesToSwitch = $"{InputFirstDoor}\ny";
+        const string inputDoorToPickAndYesToSwitch = "1\ny";
         var stringReader = new StringReader(inputDoorToPickAndYesToSwitch);
         Console.SetIn(stringReader);
 
         //Act
         _consoleGame.PlayerChooseDoor(_doors);
         _consoleGame.PlayerSwitchOrStayDoor(_doors);
-        var doorPickedAfterSwitching = _doors.FindIndex(door => door.HasPlayerPicked());
+        var unpickedDoor = _doors[0].HasPlayerPicked();
 
         //Assert
-        Assert.InRange(doorPickedAfterSwitching, SecondDoorIndex,ThirdDoorIndex);
+        Assert.False(unpickedDoor);
     }
     
+    [Fact]
+    public void GivenPlayerSwitchOrStayDoorIsCalled_WhenADoorIsAlreadyPicked_ThenOneOfTheOtherDoorsWillBePicked()
+    {
+        //Arrange
+        const string inputDoorToPickAndYesToSwitch = "1\ny";
+        var stringReader = new StringReader(inputDoorToPickAndYesToSwitch);
+        Console.SetIn(stringReader);
+
+        //Act
+        _consoleGame.PlayerChooseDoor(_doors);
+        _consoleGame.PlayerSwitchOrStayDoor(_doors);
+        var pickedDoorAfterSwitch = _doors[1].HasPlayerPicked() || _doors[2].HasPlayerPicked();
+
+        //Assert
+        Assert.True(pickedDoorAfterSwitch);
+    }
+
     [Fact]
     public void GivenGetGameOutComeIsCalled_WhenAPlayerHasPickedTheCarDoorAndHasOptedNotToSwitch_ThenThePlayerHasWonTheCar()
     {
         //Arrange
-        var mockRandom = new Mock<IRandom>();
-        var gameMaster = new GameMaster(mockRandom.Object);
-
-        const string inputDoorToPickAndNoToSwitch = $"{InputFirstDoor}\nn";
+        const string inputDoorToPickAndNoToSwitch = "1\nn";
         var stringReader = new StringReader(inputDoorToPickAndNoToSwitch);
         Console.SetIn(stringReader);
-        mockRandom.Setup(num => num.GetNumberBetweenRange(It.IsAny<int>(), It.IsAny<int>())).Returns(CarDoorIndex);
-        _doors = gameMaster.GetDoorsIncludingCarDoors();
-    
+
         //Act
         _consoleGame.PlayerChooseDoor(_doors);
         _consoleGame.PlayerSwitchOrStayDoor(_doors);
-        var winOrLoss = _consoleGame.GetGameOutCome(_doors);
+        var hasWon = _consoleGame.GetGameOutCome(_doors);
 
         //Assert
-        Assert.True(winOrLoss);
+        Assert.True(hasWon);
+    }
+    
+    [Fact]
+    public void GivenGetGameOutComeIsCalled_WhenAPlayerHasPickedTheCarDoorAndHasOptedToSwitch_ThenThePlayerHasNotWonTheCar()
+    {
+        //Arrange
+        const string inputDoorToPickAndNoToSwitch = "1\ny";
+        var stringReader = new StringReader(inputDoorToPickAndNoToSwitch);
+        Console.SetIn(stringReader);
+
+        //Act
+        _consoleGame.PlayerChooseDoor(_doors);
+        _consoleGame.PlayerSwitchOrStayDoor(_doors);
+        var hasWon = _consoleGame.GetGameOutCome(_doors);
+
+        //Assert
+        Assert.False(hasWon);
+    }
+    
+    [Fact]
+    public void GivenADoorIsOpened_WhenAPlayerHasPickedTheCarDoorAndHasOptedToSwitch_ThenThePlayerCannotSwitchToTheOpenedDoor()
+    {
+        //Arrange
+        const string inputDoorToPickAndNoToSwitch = "1\ny";
+        var stringReader = new StringReader(inputDoorToPickAndNoToSwitch);
+        Console.SetIn(stringReader);
+        _doors[1].OpeningDoor();
+
+        //Act
+        _consoleGame.PlayerChooseDoor(_doors);
+        _consoleGame.PlayerSwitchOrStayDoor(_doors);
+        var onlyDoorAvailableToBePickedAfterSwitch = _doors[2].HasPlayerPicked();
+
+        //Assert
+        Assert.True(onlyDoorAvailableToBePickedAfterSwitch);
     }
 
 }
